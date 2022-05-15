82db5863ad27c3b1da45fff5624f7e1bbfdb83ba
yeonwooRoh <101380893+yeonwooRoh@users.noreply.github.com>
2022-05-15 21:25:54 +0900

    Update 1.py

--- 1.py
+++ 1.py
@@ -11,25 +11,5 @@ while x%y!= 0:
 ablcm =a*b//y 
 #a와 b의 곱을 a와 b의 최대공약수로 나누어 a와 b의 최소공배수 구함.
 
-
-	
-while y%z != 0:	
-	temp = y%z
-	y = z
-	z = temp 
-#while문 실행 결과, a와 b의 최대공약수와 c의 최대공약수 구하여 z에 대입.
-
-tem=c
-tlcm=ablcm
-
-while tlcm%tem!= 0:
-	temp = tlcm%tem
-	tlcm=tem
-	tem=temp
-#a와 b의 최소 공배수와 c의 최대 공약수 구하여 tem에 대입
-
-lcm=ablcm*c//tem 
-#a,b의 최소 공배수와 c의 최소공배수 구함.
-
-print(z,lcm)
+print(lcm)
 
